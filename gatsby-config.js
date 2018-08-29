dcb81d9f945f45e4247ce8ad48630bcb47c6379e
Chad Fawcett <me@chadf.ca>
2018-08-29 13:32:53 -0700

    Add Netlify CMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,11 @@ module.exports = {
       },
     },
     'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ],
 }
---
+++ src/cms/cms.js
@@ -0,0 +1,10 @@
+import CMS from 'netlify-cms'
+
+//  Import component styles? Might not need it if using styled-components
+// import 'preview.css'
+
+//  Import Class component. Might include styles
+// import ClassWidget from '../components/ClassWidget'
+
+//  Register the imported widget
+// CMS.registerPreviewTemplate
