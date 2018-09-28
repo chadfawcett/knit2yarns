f46f750b7b29f161f065be8a467b64af1fe39222
Chad Fawcett <me@chadf.ca>
2018-09-28 10:50:04 -0700

    Remove contact info from header and update logo

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,7 @@
 import React from 'react'
 //  TODO: Should we use Link instead of anchor tags for relative links?
 // import { Link } from 'gatsby'
+import logo from '../img/knit2-logo-purple.png'
 
 const Nav = () => (
   <div className="nav-container">
@@ -9,50 +10,8 @@ const Nav = () => (
         <div className="row">
           <div className="col-sm-12 text-center">
             <a href="index.html">
-              <img
-                className="logo logo-dark"
-                alt="Logo"
-                src="img/knit2-logo.png"
-              />
+              <img className="logo logo-dark" alt="Logo" src={logo} />
             </a>
-          </div>
-
-          <div className="col-sm-12 text-center nav-container__sub-header">
-            <span className="vjs-1454979028076-4 vir" vic="vjs-1454979028076-4">
-              <a
-                href="https://maps.google.com/maps?q=40+1967+Trans-Canada+Hwy,+Kamloops,+BC"
-                target="blank"
-              >
-                <i
-                  className="icon icon_pin vjs-1454979028076-5"
-                  vic="vjs-1454979028076-5"
-                />{' '}
-                40-1967 Trans Canada Hwy E<br />
-                Kamloops BC
-              </a>
-            </span>
-          </div>
-          <div className="col-sm-12 text-center nav-container__sub-header">
-            <span className="vjs-1454979028076-4 vir" vic="vjs-1454979028076-4">
-              <i
-                className="icon icon_clock_alt vjs-1454979028076-5"
-                vic="vjs-1454979028076-5"
-              />{' '}
-              Mon-Fri 9:30am-5:30pm; Sat 10:00am-5:00pm
-              <br />
-              Closed Sundays and Statutory holidays
-            </span>
-          </div>
-          <div className="col-sm-12 text-center nav-container__sub-header">
-            <span className="vjs-1454979028076-4 vir" vic="vjs-1454979028076-4">
-              <i
-                className="icon icon_envelope vjs-1454979028076-5"
-                vic="vjs-1454979028076-5"
-              />{' '}
-              250-314-0276
-              <br />
-              contact@knit2yarns.com
-            </span>
           </div>
         </div>
 
